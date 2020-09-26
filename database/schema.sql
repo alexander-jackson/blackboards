4a1c2dfd4646f01e3ac7bf8f5b130d044e58b239
Alexander Jackson <alexanderjackson@protonmail.com>
2020-09-26 12:59:58 +0100

    Add schema file

---
+++ database/schema.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS sessions;
+CREATE TABLE sessions (
+	id INTEGER PRIMARY KEY,
+	title TEXT,
+	start_time TEXT,
+	remaining INTEGER
+);
+
+DROP TABLE IF EXISTS requests;
+CREATE TABLE requests (
+	session_id INTEGER,
+	email TEXT PRIMARY KEY,
+	name TEXT NOT NULL,
+	identifier INTEGER NOT NULL
+);
+
+DROP TABLE IF EXISTS registrations;
+CREATE TABLE registrations (
+	session_id INTEGER,
+	email TEXT PRIMARY KEY,
+	name TEXT NOT NULL
+);
