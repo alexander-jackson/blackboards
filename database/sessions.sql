8097b761c01a876fea7d4f949a924e4f180fef89
Alexander Jackson <alexanderjackson@protonmail.com>
2020-09-26 13:00:18 +0100

    Add sessions file

---
+++ database/sessions.sql
@@ -0,0 +1,31 @@
+INSERT INTO sessions VALUES (
+	23104, "Beginners", "Mon 28th 12:15", 6
+);
+
+INSERT INTO sessions VALUES (
+	36058, "Beginners", "Mon 28th 13:45", 10
+);
+
+INSERT INTO sessions VALUES (
+	51116, "Beginners", "Wed 30th 13:45", 6
+);
+
+INSERT INTO sessions VALUES (
+	73963, "Intermediate", "Wed 30th 15:30", 10
+);
+
+INSERT INTO sessions VALUES (
+	76637, "Beginners", "Thu 1st 12:15", 6
+);
+
+INSERT INTO sessions VALUES (
+	81562, "Intermediate", "Thu 1st 13:45", 10
+);
+
+INSERT INTO sessions VALUES (
+	82800, "Beginners (Female Only)", "Sat 3rd 11:30", 10
+);
+
+INSERT INTO sessions VALUES (
+	87415, "Intermediate", "Sat 3rd 13:00", 10
+);
