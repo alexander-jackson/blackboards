5bca8b8e1bcfea12ad8bb989ee2fd06eee599df4
Alexander Jackson <alexanderjackson@protonmail.com>
2021-03-12 09:24:15 +0000

    style: add logging output for Rocket
    Add `rocket` to the logging levels and allow it to output debug level
    messages.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 fn main() {
     dotenv::dotenv().unwrap();
 
-    let filters = vec![("blackboards", log::LevelFilter::Trace)];
+    let filters = vec![
+        ("blackboards", log::LevelFilter::Trace),
+        ("rocket", log::LevelFilter::Debug),
+    ];
     blackboards::setup_logger_with_filters(filters);
 
     let rocket = blackboards::build_rocket();
