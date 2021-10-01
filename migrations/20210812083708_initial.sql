3d05c3519944a70fc16bfef90ce471d1caad5e6e
Alexander Jackson <alex.jackson@starlingbank.com>
2021-10-01 21:11:14 +0100

    migrations: modify initial setup
    Modify the initial setup to work in production as well, as the tables
    already existed in this state beforehand.

--- migrations/20210812083708_initial.sql
+++ migrations/20210812083708_initial.sql
@@ -1,11 +1,11 @@
-CREATE TABLE sessions (
+CREATE TABLE IF NOT EXISTS sessions (
 	id INTEGER PRIMARY KEY,
 	title TEXT,
 	start_time BIGINT,
 	remaining INTEGER
 );
 
-CREATE TABLE registrations (
+CREATE TABLE IF NOT EXISTS registrations (
 	session_id INTEGER,
 	warwick_id INTEGER,
 	name TEXT NOT NULL,
@@ -16,7 +16,7 @@ CREATE TABLE registrations (
 	ON DELETE CASCADE
 );
 
-CREATE TABLE attendances (
+CREATE TABLE IF NOT EXISTS attendances (
 	session_id INTEGER,
 	warwick_id INTEGER,
 	PRIMARY KEY (session_id, warwick_id),
@@ -26,12 +26,12 @@ CREATE TABLE attendances (
 	ON DELETE CASCADE
 );
 
-CREATE TABLE auth_pairs (
+CREATE TABLE IF NOT EXISTS auth_pairs (
 	token TEXT PRIMARY KEY,
 	secret TEXT
 );
 
-CREATE TABLE personal_bests (
+CREATE TABLE IF NOT EXISTS personal_bests (
 	warwick_id INTEGER PRIMARY KEY,
 	name TEXT NOT NULL,
 	squat REAL,
@@ -43,26 +43,26 @@ CREATE TABLE personal_bests (
 	show_wl BOOLEAN
 );
 
-CREATE TABLE exec_positions (
+CREATE TABLE IF NOT EXISTS exec_positions (
 	id INTEGER PRIMARY KEY,
 	title TEXT NOT NULL,
 	num_winners INTEGER NOT NULL,
 	open BOOLEAN NOT NULL
 );
 
-CREATE TABLE candidates (
+CREATE TABLE IF NOT EXISTS candidates (
 	warwick_id INTEGER PRIMARY KEY,
 	name TEXT NOT NULL,
 	elected BOOLEAN NOT NULL
 );
 
-CREATE TABLE nominations (
+CREATE TABLE IF NOT EXISTS nominations (
 	position_id INTEGER NOT NULL,
 	warwick_id INTEGER NOT NULL,
 	PRIMARY KEY (position_id, warwick_id)
 );
 
-CREATE TABLE votes (
+CREATE TABLE IF NOT EXISTS votes (
 	warwick_id INTEGER NOT NULL,
 	position_id INTEGER NOT NULL,
 	candidate_id INTEGER NOT NULL,
